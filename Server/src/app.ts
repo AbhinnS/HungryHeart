2f387c3d68e6034254312ab265e2a8530336f0a6
Abhinn Sayansh <115138018+AbhinnS@users.noreply.github.com>
2026-07-04 01:04:46 +0530

    Update app.ts

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(
   cors({
     //origin: "http://localhost:5173",
-    origin: "https://hungryheart.in/",
+    origin: "https://hungryheart.in",
     credentials: true,
   })
 );
