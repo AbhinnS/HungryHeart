a517dab8e22bd6c422bbacfea3d497ac27ce1f0a
Abhinn Sayansh <115138018+AbhinnS@users.noreply.github.com>
2026-07-04 01:52:23 +0530

    Update emailService.ts

--- Server/src/utils/emailService.ts
+++ Server/src/utils/emailService.ts
@@ -19,10 +19,7 @@ const isSmtpConfigured = () =>
 //   });
 const createTransporter = () =>
   nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT || 587),
-    secure: process.env.SMTP_SECURE === "true",
-    family: 4, // 👈 Force IPv4
+    service: "gmail",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
